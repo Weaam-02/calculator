7b37571003f0f2b6ec17f9a575205403c9804f84
WeaamRashed <s444015412@st.uqu.edu.sa>
2024-07-10 18:54:18 +0300

    buttons using grid

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,31 +1,4 @@
 <!-- <h1>Welcome to My Cute Calc</h1>
-<div>
-  <p></p>
-  <button>%</button>
-  <button>√</button>
-  <button>CE</button>
-  <button>C</button>
-
-  <button>7</button>
-  <button>8</button>
-  <button>9</button>
-  <button>-</button>
-
-  <button>4</button>
-  <button>5</button>
-  <button>6</button>
-  <button>/</button>
-
-  <button>1</button>
-  <button>2</button>
-  <button>3</button>
-  <button>x</button>
-
-  <button>.</button>
-  <button>0</button>
-  <button>=</button>
-  <button>+</button>
-</div> -->
 <!-- <script lang="ts">
   let x = 1;
 
@@ -59,8 +32,29 @@
   <title>My Cute Calc</title>
 </svelte:head>
 
-<div
-  class="bg-white w-[20rem] h-[30rem] rounded-2xl flex justify-center items-center"
->
-  <div class="bg-slate-100 w-40 h-40 rounded-full"></div>
+<div class="bg-white rounded-2xl grid grid-cols-4 gap-1">
+  <button> % </button>
+  <button> √ </button>
+  <button> CE </button>
+  <button> C </button>
+
+  <button> 7 </button>
+  <button> 8 </button>
+  <button> 9 </button>
+  <button> - </button>
+
+  <button> 4 </button>
+  <button> 5 </button>
+  <button> 6 </button>
+  <button> ÷ </button>
+
+  <button> 1 </button>
+  <button> 2 </button>
+  <button> 3 </button>
+  <button> x </button>
+
+  <button> . </button>
+  <button> 0 </button>
+  <button> = </button>
+  <button> + </button>
 </div>
