f8335875be164c974a677f66c1fab0579e1c5ab4
WeaamRashed <s444015412@st.uqu.edu.sa>
2024-07-10 18:13:28 +0300

    centered white rectangle

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,66 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<!-- <h1>Welcome to My Cute Calc</h1>
+<div>
+  <p></p>
+  <button>%</button>
+  <button>√</button>
+  <button>CE</button>
+  <button>C</button>
+
+  <button>7</button>
+  <button>8</button>
+  <button>9</button>
+  <button>-</button>
+
+  <button>4</button>
+  <button>5</button>
+  <button>6</button>
+  <button>/</button>
+
+  <button>1</button>
+  <button>2</button>
+  <button>3</button>
+  <button>x</button>
+
+  <button>.</button>
+  <button>0</button>
+  <button>=</button>
+  <button>+</button>
+</div> -->
+<!-- <script lang="ts">
+  let x = 1;
+
+  function multipy() {
+    x *= 2;
+  }
+
+  function divide() {
+    x /= 2;
+  }
+</script>
+
+<div class="bg-black mt-10 mr-10 ml-10 border-red-400 border-4 rounded">
+  <h1 class="text-5xl text-center text-red-400 mt-10 mb-10">Counter</h1>
+  <h1 class="text-5xl text-center text-red-300 mb-10">|| {x} ||</h1>
+  <button
+    on:click={multipy}
+    class="text-4xl text-center text-red-400 border-red-400 border-4 mr-48 ml-96 mb-10 p-2"
+  >
+    Multipy
+  </button>
+  <button
+    on:click={divide}
+    class="text-4xl text-center text-red-400 border-red-400 border-4 ml-96 p-2"
+  >
+    Divide
+  </button>
+</div> -->
+<!-- .................................................. -->
+<svelte:head>
+  <title>My Cute Calc</title>
+</svelte:head>
+
+<div
+  class="bg-white w-[20rem] h-[30rem] rounded-2xl flex justify-center items-center"
+>
+  <div class="bg-slate-100 w-40 h-40 rounded-full"></div>
+</div>
