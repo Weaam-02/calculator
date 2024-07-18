aed96d13724a846790b1c0fd1ea8f9b22683eb34
WeaamRashed <s444015412@st.uqu.edu.sa>
2024-07-18 17:52:14 +0300

    everything is working nom

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -28,33 +28,110 @@
   </button>
 </div> -->
 <!-- .................................................. -->
+
+<script lang="ts">
+  import BackSpace from "$lib/icons/backSpace.svelte";
+  import Divisionicon from "$lib/icons/division.svelte";
+  import Plus from "$lib/icons/plus.svelte";
+  import Multi from "$lib/icons/multi.svelte";
+  import Minus from "$lib/icons/minus.svelte";
+  import Equals from "$lib/icons/equals.svelte";
+
+  function addToEquation(value: string) {
+    equation += value;
+  }
+
+  // TODO: fix remove space
+  function backSpace() {
+    equation = equation.substring(0, equation.length - 1);
+  }
+
+  function clear() {
+    equation = "";
+  }
+
+  function solve() {
+    equation = eval(equation);
+  }
+
+  let equation: string = "";
+</script>
+
 <svelte:head>
   <title>My Cute Calc</title>
 </svelte:head>
 
-<div class="bg-white rounded-2xl grid grid-cols-4 gap-1">
-  <button> % </button>
-  <button> √ </button>
-  <button> CE </button>
-  <button> C </button>
+<div
+  class="bg-white rounded-2xl grid grid-cols-4 gap-2 p-7 text-2xl font-semibold shadow-2xl"
+>
+  <div
+    class="bg-blue-400 h-14 rounded-full flex items-center px-6 my-4 text-white col-span-4 shadow-lg shadow-blue-400/50"
+  >
+    {equation}
+  </div>
 
-  <button> 7 </button>
-  <button> 8 </button>
-  <button> 9 </button>
-  <button> - </button>
+  <!-- "bg-slate-800 text-white hover:bg-slate-900 shadow-slate-800/80 -->
 
-  <button> 4 </button>
-  <button> 5 </button>
-  <button> 6 </button>
-  <button> ÷ </button>
+  <button on:click={clear} class="bg-slate-100 hover:bg-slate-200/50">
+    AC
+  </button>
 
-  <button> 1 </button>
-  <button> 2 </button>
-  <button> 3 </button>
-  <button> x </button>
+  <button on:click={backSpace} class="bg-slate-100 hover:bg-slate-200/50">
+    <BackSpace />
+  </button>
 
-  <button> . </button>
-  <button> 0 </button>
-  <button> = </button>
-  <button> + </button>
+  <button
+    on:click={() => addToEquation(" /100 ")}
+    class="bg-slate-100 hover:bg-slate-200/50 font-extrabold"
+  >
+    %
+  </button>
+
+  <button
+    on:click={() => addToEquation(" + ")}
+    class="bg-green-400 text-white hover:bg-green-500/80 shadow-green-400/80"
+  >
+    <Plus />
+  </button>
+
+  <button on:click={() => addToEquation("7")}> 7 </button>
+  <button on:click={() => addToEquation("8")}> 8 </button>
+  <button on:click={() => addToEquation("9")}> 9 </button>
+  <button
+    on:click={() => addToEquation(" - ")}
+    class="bg-red-500 text-white hover:bg-red-600/90 shadow-red-500/60"
+  >
+    <Minus />
+  </button>
+
+  <button on:click={() => addToEquation("4")}> 4 </button>
+  <button on:click={() => addToEquation("5")}> 5 </button>
+  <button on:click={() => addToEquation("6")}> 6 </button>
+  <button
+    on:click={() => addToEquation(" / ")}
+    class="bg-blue-500 text-white hover:bg-blue-600/90 shadow-blue-500/80"
+  >
+    <Divisionicon />
+  </button>
+
+  <button on:click={() => addToEquation("1")}> 1 </button>
+  <button on:click={() => addToEquation("2")}> 2 </button>
+  <button on:click={() => addToEquation("3")}> 3 </button>
+  <button
+    on:click={() => addToEquation(" * ")}
+    class="bg-yellow-400 text-white hover:bg-yellow-400/90 shadow-yellow-400/80"
+  >
+    <Multi />
+  </button>
+
+  <button on:click={() => addToEquation(" . ")} class="font-extrabold">
+    .
+  </button>
+  <button on:click={() => addToEquation("0")}> 0 </button>
+  <button
+    on:click={() => solve()}
+    class="bg-slate-100 hover:bg-slate-200/50 col-span-2"
+  >
+    <Equals />
+  </button>
 </div>
